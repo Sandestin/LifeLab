83b741495338a612405c4e2678d9c88f81936584
Jon Attfield <jon.attfield@gmail.com>
2021-01-08 06:51:51 +0000

    Initial Commit

---
+++ Shared/LifeLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLabApp.swift
+//  Shared
+//
+//  Created by Jonathan Attfield on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LifeLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
